f298f79cd922686841614b769eda515de2341174
andrecaindaa <andre.cainda@trainee.inovcorp.com>
2025-12-24 19:31:35 +0000

    Fix MessageSent event typehint to use Message model

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcast
+{
+    use Dispatchable, SerializesModels;
+
+    public Message $message;
+
+    public function __construct(Message $message)
+    {
+        $this->message = $message->load('user');
+    }
+
+    public function broadcastOn()
+    {
+        return new PrivateChannel('room.' . $this->message->room_id);
+    }
+}
+
