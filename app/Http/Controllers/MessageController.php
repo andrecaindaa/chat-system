63fd525f7693ae22839b7900ff7a95f9c12225fe
andrecaindaa <andre.cainda@trainee.inovcorp.com>
2025-12-24 16:06:56 +0000

    Criar mensagens associadas ao utilizador

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,20 +9,16 @@ class MessageController extends Controller
 {
     public function store(Request $request, Room $room)
     {
-        abort_unless(
-            $room->users()->where('user_id', auth()->id())->exists(),
-            403
-        );
-
         $validated = $request->validate([
-            'body' => ['required', 'string'],
+            'content' => ['required', 'string'],
         ]);
 
         $room->messages()->create([
-            'body' => $validated['body'],
-            'sender_id' => auth()->id(),
+            'user_id' => auth()->id(),
+            'content' => $validated['content'],
         ]);
 
         return back();
     }
+
 }
